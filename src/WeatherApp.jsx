3e4bde7edf184a63e5499d6492d31d6042bd07e5
Pablo Chorda <pablochorda365@gmail.com>
2024-07-18 19:01:15 +0200

    Hemos echo una app de clima

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+export const WeatherApp = () => {
+
+    const urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+    const API_KEY = '31df102cf469e3eb20994712a1ec7c28'
+    const difKelvin = 273.15
+
+    const [ciudad, setCiudad] = useState('')
+    const [dataClima, setDataClima] = useState(null)
+
+    const handleCambioCiudad = (e) => {
+        setCiudad(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(ciudad.length > 0) fetchClima()
+    }
+
+    const fetchClima = async () => {
+        try{
+            const response = await fetch(`${urlBase}?q=${ciudad}&appid=${API_KEY}&lang=es`)
+            const data = await response.json()
+            setDataClima(data)
+        }catch(error){
+            console.error('Ocurrió el siguiente problema: ', error)
+        }
+    }
+
+
+    return (
+    <div className="container">
+
+        <h1>Aplicación del Clima</h1>
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="text"
+            value={ciudad}
+            onChange={handleCambioCiudad}
+            />
+            <button type="submit">Buscar</button>
+        </form>
+        {
+            dataClima && (
+                <div>
+                    <h2>{dataClima.name}</h2>
+                    <p>Temperatura: {parseInt(dataClima?.main?.temp - difKelvin)}ºC</p>
+                    <p>Condición meteorológica: {dataClima.weather[0].description}</p>
+                    <img src={`https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`} />
+                </div>
+            )
+        }
+    </div>
+    )
+}
